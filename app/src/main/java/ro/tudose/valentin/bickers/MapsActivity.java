d1a401da3c515f5f04cf2ae530da336f6307956f
Tudose Valentin <tudose.valentin21@gmail.com>
2018-11-23 18:11:44 +0200

    Still playing with google Maps

--- app/src/main/java/ro/tudose/valentin/bickers/MapsActivity.java
+++ app/src/main/java/ro/tudose/valentin/bickers/MapsActivity.java
@@ -1,7 +1,17 @@
 package ro.tudose.valentin.bickers;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,17 +22,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    private PositionManager positionManager;
+    public static double MY_LATITUDE = 44.4411548;
+    public static double MY_LONGITUDE = 26.0570443;
+    public static String LOG_TAG = "BICKERS: ";
+
+
+    private GoogleMap googleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        positionManager = new PositionManager(this);
     }
 
 
@@ -37,14 +50,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(positionManager.getLatitude(), positionManager.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        this.googleMap = googleMap;
+        initGPS();
     }
 
-    public static void updatePostion(double latitude,double longitude) {
+    private void initGPS() {
+        final Context mActivity = this;
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        final LatLng position = new LatLng(MY_LATITUDE, MY_LONGITUDE);
+        googleMap.addMarker(new MarkerOptions().position(position));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        googleMap.setMinZoomPreference(10);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d(LOG_TAG,"[" + String.valueOf(location.getLatitude())+","+ String.valueOf(location.getLongitude()) + "]");
+                Toast.makeText(mActivity,"[" + String.valueOf(location.getLatitude())+","+ String.valueOf(location.getLongitude()) + "]",Toast.LENGTH_LONG).show();
+                final LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+                googleMap.setMinZoomPreference(googleMap.getMaxZoomLevel());
+                googleMap.addMarker(new MarkerOptions().position(position));
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                //TODO popup to activate gps
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+
+        if (locationManager != null) {
+            locationManager.requestLocationUpdates("gps", 100, 0.1f, locationListener);
+        }
     }
 }
